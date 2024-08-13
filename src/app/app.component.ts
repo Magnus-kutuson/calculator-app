7019a2da82647d683bf07079b7f67b28ffcba650
Magnus <abeikumagnus@gmail.com>
2024-08-13 14:22:34 -0700

    edit page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalculatorComponent } from "./calculator/calculator.component";
-import { ThemeSwitcherComponent } from "./theme-switcher/theme-switcher.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CalculatorComponent, ThemeSwitcherComponent],
+  imports: [RouterOutlet, CalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
